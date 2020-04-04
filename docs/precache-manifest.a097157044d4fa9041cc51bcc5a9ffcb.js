47e2d217752cbe2b1afe6234ef0b6b18d000d581
Ebrahim Merchant <thunderblazerx@gmail.com>
2020-04-04 01:40:58 -0400

    Adding github pages build

---
+++ docs/precache-manifest.a097157044d4fa9041cc51bcc5a9ffcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9692fc7ac525b44c5e2a2b2476d09b67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3406fab2f7c115e50bc",
+    "url": "/static/css/main.f57bb489.chunk.css"
+  },
+  {
+    "revision": "5f54524ac80c5ee5fc7b",
+    "url": "/static/js/2.e9a870c2.chunk.js"
+  },
+  {
+    "revision": "d3406fab2f7c115e50bc",
+    "url": "/static/js/main.d3ce4caa.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  }
+]);
